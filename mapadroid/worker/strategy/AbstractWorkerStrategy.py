06775a3ba0e4cf60db63f79554bf4a048d571a36
Andrew Lawson <1607258+Boby360@users.noreply.github.com>
2023-03-05 10:50:52 -0800

    Update AbstractWorkerStrategy.py

--- mapadroid/worker/strategy/AbstractWorkerStrategy.py
+++ mapadroid/worker/strategy/AbstractWorkerStrategy.py
@@ -339,12 +339,12 @@ class AbstractWorkerStrategy(ABC):
             elif screen_type == ScreenType.MAINTENANCE:
                 logger.warning("Maintenance screen - switch account ...")
                 self._worker_state.login_error_count += 1
-                    if self._worker_state.login_error_count < 2:
-                        await self._switch_user()
-                    else:
-                        await self.stop_pogo()
-                        await asyncio.sleep(14400)
-                        self._worker_state.login_error_count = 0
+                if self._worker_state.login_error_count < 2:
+                    await self._switch_user()
+                else:
+                    await self.stop_pogo()
+                    await asyncio.sleep(14400)
+                    self._worker_state.login_error_count = 0
             elif screen_type in [ScreenType.ERROR, ScreenType.FAILURE]:
                 logger.warning('Something wrong with screendetection or pogo failure screen')
                 self._worker_state.login_error_count += 1
